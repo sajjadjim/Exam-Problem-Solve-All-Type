bf7d0a422775c060fc866115abbeced4553404b2
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2024-04-12 19:53:17 +0600

    Update 4 Solution.py

--- Object Oriented Programing 2/Midterm spring 2024/4 Solution.py
+++ Object Oriented Programing 2/Midterm spring 2024/4 Solution.py
@@ -2,7 +2,7 @@ Height = {'Child':[30, 40 ,35 , 45 , 30] ,
           'Teenage': [50 , 60 , 55 , 65 ,60] ,
           'Adult': [85 , 90 , 92 ,88 ,82],
           }
- 
+   
 avarage_Height = {}
 for age_list ,Height_list in Height.items():
 #new arguments = sum of (All Key elements )/ length of Key elements
