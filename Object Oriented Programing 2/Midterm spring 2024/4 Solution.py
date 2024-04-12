40379b7d5f0a99aa52162391970cfce63b2b3da9
SAJJAD HOSAIN JIM <120739140+sajjadjim@users.noreply.github.com>
2024-04-12 19:53:41 +0600

    Update 4 Solution.py

--- Object Oriented Programing 2/Midterm spring 2024/4 Solution.py
+++ Object Oriented Programing 2/Midterm spring 2024/4 Solution.py
@@ -6,7 +6,7 @@ Height = {'Child':[30, 40 ,35 , 45 , 30] ,
 avarage_Height = {}
 for age_list ,Height_list in Height.items():
 #new arguments = sum of (All Key elements )/ length of Key elements
-    avarage_Heights = sum(Height_list) / len(Height_list)
+    avarage_Heights = sum(Height_list) / len(Height_list) 
     avarage_Height[age_list] = avarage_Heights
 
 print(avarage_Height) 
